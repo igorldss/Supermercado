1888c79aea6cca5ca86fdd91f0d1453c0ef2a155
Igor Santos <106188518+igorldss@users.noreply.github.com>
2022-12-26 20:47:57 -0300

    Update intefaceSupermercado.py
    Continuo a criar a interface, o acesso e as funções estão prontas, o desafio atual tem a ver com a implantação dos métodos da forma como eu quero visualizar

--- intefaceSupermercado.py
+++ intefaceSupermercado.py
@@ -19,13 +19,5 @@ janela.title("Lista de Fornecedores e Produtos")
 texto_inicial = Label(janela, text="Lista de fornecedores",background=fundoTela,foreground=fundoBotao)
 texto_inicial.place(x=10, y=10, width=150, height=30)
 
-#---------------------------------------------------------------------------------
-
-lb_fornecedores = Listbox(janela)
-lb_fornecedores.place(x=50, y=100, width=150, height=30)
-
-botao_fornecedores = Button(janela, text="Fornecedores",command=verforn)
-botao_fornecedores.place(x=50, y=50, width=150, height=30)
-
 
 janela.mainloop()
