5e2c58d6d4216ea347aa815ede11ea774b3e99b6
Igor Santos <106188518+igorldss@users.noreply.github.com>
2023-01-02 17:06:29 -0300

    Update funcoesSupermercado.py

--- funcoesSupermercado.py
+++ funcoesSupermercado.py
@@ -9,16 +9,16 @@ def incluirforn():
     from sqlalchemy.orm import sessionmaker
     SessaoBD = sessionmaker(bind=engine)
     sessao = SessaoBD()
-    novoforn = Fornecedores(id=int(input('Digite o id')),\
-                                nome=input('Digite o nome'),\
-                                    email=input('Digite o email'))
+    novoforn = Fornecedores(id=int(input('id')),\
+                            nome=input('name'),\
+                            email=input('email'))
     sessao.add(novoforn)
     sessao.commit()
 def excluirforn():
     from sqlalchemy.orm import sessionmaker
     SessaoBD = sessionmaker(bind=engine)
     sessao = SessaoBD()
-    excluir = sessao.query(Fornecedores).filter(Fornecedores.nome==input('Digite o nome que quer apagar')).first()
+    excluir = sessao.query(Fornecedores).filter(Fornecedores.nome==input('excprod')).first()
     if excluir != None:
         sessao.delete(excluir)
         sessao.commit()
@@ -26,16 +26,16 @@ def incluirprod():
     from sqlalchemy.orm import sessionmaker
     SessaoBD = sessionmaker(bind=engine)
     sessao = SessaoBD()
-    novoprod = Produtos(id=int(input('Digite o id')),\
-                            desc=input('Digite a descrição'),\
-                                id_fornprod=int(input('Digite o id do fornecedor do produto')))
+    novoprod = Produtos(id=int(input('id')),\
+                        desc=input('desc'),\
+                        id_fornprod=int(input('idforn')))
     sessao.add(novoprod)
     sessao.commit()
 def excluirprod():
     from sqlalchemy.orm import sessionmaker
     SessaoBD = sessionmaker(bind=engine)
     sessao = SessaoBD()
-    excluir1 = sessao.query(Produtos).filter(Produtos.desc==input('Digite a descrição do produto que quer apagar')).first()
+    excluir1 = sessao.query(Produtos).filter(Produtos.desc==input('excprod')).first()
     if excluir1 != None:
         sessao.delete(excluir1)
         sessao.commit()
@@ -45,16 +45,15 @@ def verforn():
     sessao = SessaoBD()
     resposta = sessao.query(Fornecedores)
     for resp in resposta:
-        print(resp.nome,\
-                        resp.id)
+        return resp.nome,\
+                resp.id
 def verprod():
     from sqlalchemy.orm import sessionmaker
     SessaoBD = sessionmaker(bind=engine)
     sessao = SessaoBD()
     resposta = sessao.query(Produtos)
     for resp in resposta:
-            print(resp.desc,\
-                        resp.id)
+            return resp.desc,\
+                    resp.id
 
 Base.metadata.create_all(engine) 
-
