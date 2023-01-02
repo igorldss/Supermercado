9677072965b639775a77dd05437c19ec10ac3381
Igor Santos <106188518+igorldss@users.noreply.github.com>
2023-01-02 17:07:17 -0300

    Update intefaceSupermercado.py

--- intefaceSupermercado.py
+++ intefaceSupermercado.py
@@ -2,9 +2,6 @@ from funcoesSupermercado import *
 from tkinter import *
 from tkinter import ttk
 
-def imprimir():
-    pass
-
 fundoTela = '#35a1c5'
 fundoBotao = '#022a5e'
 letra = 'white'
@@ -14,10 +11,5 @@ janela = Tk()
 janela.geometry("500x500")
 janela.configure(background=fundoTela)
 janela.title("Lista de Fornecedores e Produtos")
-
-
-texto_inicial = Label(janela, text="Lista de fornecedores",background=fundoTela,foreground=fundoBotao)
-texto_inicial.place(x=10, y=10, width=150, height=30)
-
 
 janela.mainloop()
